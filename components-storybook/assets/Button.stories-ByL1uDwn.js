626c7ad689c2a73d292ff33dbee30900e3076cf6
Marianne <wanesjitu@gmail.com>
2026-01-03 01:47:12 +0800

    Add files via upload

---
+++ components-storybook/assets/Button.stories-ByL1uDwn.js
@@ -0,0 +1,19 @@
+import{j as x}from"./jsx-runtime-Cf8x2fCZ.js";import{c as v}from"./cn-util-BlRws91P.js";import"./index-yBjzXJbu.js";const g=({className:y,size:t="medium",variant:s="primary",text:f="Click Me"})=>x.jsx("button",{className:v("rounded-md bg-[#000] font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2",t==="small"&&"px-2.5 py-1.5 text-sm",t==="medium"&&"px-4 py-2 text-base",t==="large"&&"px-6 py-3 text-lg",s==="primary"&&"bg-midnight-forest text-electric-lime hover:bg-midnight-forest-dark focus:ring-midnight-forest-dark",s==="secondary"&&"bg-gray-200 text-gray-800 hover:bg-gray-300 focus:ring-gray-400",y),children:f});g.__docgenInfo={description:"",methods:[],displayName:"Button",props:{className:{required:!1,tsType:{name:"string"},description:""},size:{required:!1,tsType:{name:"union",raw:"'small' | 'medium' | 'large'",elements:[{name:"literal",value:"'small'"},{name:"literal",value:"'medium'"},{name:"literal",value:"'large'"}]},description:"",defaultValue:{value:"'medium'",computed:!1}},variant:{required:!1,tsType:{name:"union",raw:"'primary' | 'secondary'",elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"}]},description:"",defaultValue:{value:"'primary'",computed:!1}},text:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'Click Me'",computed:!1}}}};const S={title:"Components/Button",component:g},e={args:{variant:"primary",size:"medium",text:"Primary Button"}},r={args:{variant:"secondary",size:"medium",text:"Secondary Button"}},a={args:{variant:"primary",size:"large",text:"Large Button"}};var n,o,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    size: 'medium',
+    text: 'Primary Button'
+  }
+}`,...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};var m,u,l;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary',
+    size: 'medium',
+    text: 'Secondary Button'
+  }
+}`,...(l=(u=r.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var d,c,p;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    size: 'large',
+    text: 'Large Button'
+  }
+}`,...(p=(c=a.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const z=["Primary","Secondary","Large"];export{a as Large,e as Primary,r as Secondary,z as __namedExportsOrder,S as default};
---
+++ components-storybook/assets/Headline.stories-Bcw4lMDH.js
@@ -0,0 +1,5 @@
+import{j as i}from"./jsx-runtime-Cf8x2fCZ.js";import{c}from"./cn-util-BlRws91P.js";import"./index-yBjzXJbu.js";const t=({className:n,children:o})=>i.jsx("h1",{className:c("font-bold text-[2em]",n),children:o});t.__docgenInfo={description:"",methods:[],displayName:"Headline",props:{className:{required:!1,tsType:{name:"string"},description:""},children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const l={title:"Components/Headline",component:t},e={args:{children:"This is a Headline"}};var s,a,r;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'This is a Headline'
+  }
+}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,l as default};
