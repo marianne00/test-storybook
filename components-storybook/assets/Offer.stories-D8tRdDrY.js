fbd8c30b484516a6ccb4fcabf8e8399951aae4b4
Marianne <wanesjitu@gmail.com>
2026-01-02 22:17:10 +0800

    Add files via upload

---
+++ components-storybook/assets/Offer.stories-D8tRdDrY.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{c as u}from"./index-BLy2C-Ia.js";import"./index-yBjzXJbu.js";const h=r=>e.jsxs("div",{className:u("flex flex-col gap-y-[.25em] w-[300px]",r.className),children:[e.jsx("h1",{children:r.title}),e.jsx("p",{className:"font-bold text-[2em]",children:r.rate}),e.jsx("hr",{}),e.jsx("p",{children:r.terms})]});h.__docgenInfo={description:"",methods:[],displayName:"Offer",props:{className:{required:!1,tsType:{name:"string"},description:""},title:{required:!1,tsType:{name:"string"},description:""},rate:{required:!1,tsType:{name:"string"},description:""},terms:{required:!1,tsType:{name:"string"},description:""}}};const j={title:"Component Blocks/Offer",component:h},s={args:{title:"Lease Offer",rate:"3.5% p.a.",terms:"36 months lease term"}},a={args:{title:"Finance Offer",rate:"5.0% p.a.",terms:"48 months finance term"}},t={args:{title:"Cash Back Offer",rate:"2,000 AUD",terms:"Receive $2,000 cash back on purchase"}};var n,c,o;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    title: 'Lease Offer',
+    rate: '3.5% p.a.',
+    terms: '36 months lease term'
+  }
+}`,...(o=(c=s.parameters)==null?void 0:c.docs)==null?void 0:o.source}}};var i,m,p;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    title: 'Finance Offer',
+    rate: '5.0% p.a.',
+    terms: '48 months finance term'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var f,l,d;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    title: 'Cash Back Offer',
+    rate: '2,000 AUD',
+    terms: 'Receive $2,000 cash back on purchase'
+  }
+}`,...(d=(l=t.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const k=["Lease","Finance","CashBack"];export{t as CashBack,a as Finance,s as Lease,k as __namedExportsOrder,j as default};
