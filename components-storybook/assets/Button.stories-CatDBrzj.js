c01e5a51ce8cbda2310eaf3772e9c19fc380f962
Marianne <wanesjitu@gmail.com>
2026-01-02 21:56:05 +0800

    Add files via upload

---
+++ components-storybook/assets/Button.stories-CatDBrzj.js
@@ -0,0 +1,13 @@
+import{j as p}from"./jsx-runtime-Cf8x2fCZ.js";import{c as y}from"./cn-util-BlRws91P.js";import"./index-yBjzXJbu.js";const u=({className:d,size:a="medium",variant:t="primary",text:c="Click Me"})=>p.jsx("button",{className:y("rounded-md bg-[#000] font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2",a==="small"&&"px-2.5 py-1.5 text-sm",a==="medium"&&"px-4 py-2 text-base",a==="large"&&"px-6 py-3 text-lg",t==="primary"&&"bg-midnight-forest text-electric-lime hover:bg-midnight-forest-dark focus:ring-midnight-forest-dark",t==="secondary"&&"bg-gray-200 text-gray-800 hover:bg-gray-300 focus:ring-gray-400",d),children:c});u.__docgenInfo={description:"",methods:[],displayName:"Button",props:{className:{required:!1,tsType:{name:"string"},description:""},size:{required:!1,tsType:{name:"union",raw:"'small' | 'medium' | 'large'",elements:[{name:"literal",value:"'small'"},{name:"literal",value:"'medium'"},{name:"literal",value:"'large'"}]},description:"",defaultValue:{value:"'medium'",computed:!1}},variant:{required:!1,tsType:{name:"union",raw:"'primary' | 'secondary'",elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"}]},description:"",defaultValue:{value:"'primary'",computed:!1}},text:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'Click Me'",computed:!1}}}};const v={title:"Components/Button",component:u},e={args:{variant:"primary",size:"medium",text:"Primary Button"}},r={args:{variant:"secondary",size:"medium",text:"Secondary Button"}};var s,n,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    size: 'medium',
+    text: 'Primary Button'
+  }
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var i,m,l;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary',
+    size: 'medium',
+    text: 'Secondary Button'
+  }
+}`,...(l=(m=r.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const b=["Primary","Secondary"];export{e as Primary,r as Secondary,b as __namedExportsOrder,v as default};
